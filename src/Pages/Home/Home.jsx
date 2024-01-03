603e156d4f838ee1ec34c316a7812f84ea6be3d7
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-01-03 12:02:02 +0530

    add count up package

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Home.scss";
+import CountUp from 'react-countup';
 import { dashBoardArray } from "../../utils/helper";
 
 const Home = () => {
   return (
     <div className="d-flex w-100 align-items-center text-center justify-content-center">
-      <div className="d-flex flex-wrap gap-3">
+      <div className="d-flex card-container flex-wrap gap-3">
         {dashBoardArray.map((data, index) => {
           return (
             <div
@@ -13,7 +14,7 @@ const Home = () => {
               style={{ backgroundColor: "#1c1c1e" }}
               className="dashboard_cards card d-flex justify-content-center align-items-center gap-1 flex-column"
             >
-              <h2 style={{ color: data.color }}>{data.value}</h2>
+              <h2 style={{ color: data.color }}><CountUp end={data.value} useGrouping={false} duration={"2"} /></h2>
               <h5 style={{ color: "#f9f9f9", fontSize: "16px" }}>{data.key}</h5>
             </div>
           );
