2e19377a02234bad2ae52acf5ceec4eb3933a503
Rachit Patel <rachitp509@gmail.com>
2024-02-27 10:12:47 +0530

    added baseURL

--- src/utils/axiosUtil.js
+++ src/utils/axiosUtil.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  // baseURL: "http://localhost:4000",
   // baseURL:"https://string-geo.adaptable.app"
+  baseURL: "https://string-geo-backend-5wqk.onrender.com",
 });
 
 export default axiosInstance;
