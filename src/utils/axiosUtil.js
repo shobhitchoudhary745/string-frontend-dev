5b51dde90934bd0e9ce7b3b2e3d535fdef156d62
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-03-08 08:48:19 +0530

    changes

--- src/utils/axiosUtil.js
+++ src/utils/axiosUtil.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:4000",
-  // baseURL:"https://string-geo.adaptable.app"
-  baseURL: "https://string-geo-backend-5wqk.onrender.com",
+  baseURL:"https://api.stringevolve.in"
+  // baseURL: "https://string-geo-backend-5wqk.onrender.com",
 });
 
 export default axiosInstance;
